7da1a27b48b7e2fe0395cec1a6d4787e69e5219a
francescochimenti <127688138+francescochimenti@users.noreply.github.com>
2023-05-27 17:21:28 +0200

    m2d2

---
+++ Corso/M2/D2/script.js
@@ -0,0 +1,35 @@
+let calcolo = 12 + 20;
+
+console.log(calcolo);
+
+let x = 12;
+
+console.log(x);
+
+let nome = "Francesco";
+
+console.log(nome);
+
+let y;
+
+y = x - 4;
+
+console.log(y);
+
+let me = {
+    nome: "Francesco",
+    cognome: "Chimenti",
+    hobby: "Programmazione"
+};
+
+console.log(me);
+
+let name1 = "john";
+let name2 = "John";
+
+console.log(name1 !== name2);
+
+console.log(name1.toLowerCase() === name2.toLowerCase()); 
+
+
+
