c082e396a4a345bb02bdcd5d5adedc32d039364a
francescochimenti <127688138+francescochimenti@users.noreply.github.com>
2023-07-02 12:07:12 +0200

    add new js

--- Corso/M3/D4/script.js
+++ Corso/M3/D4/script.js
@@ -1,12 +1,32 @@
-document.querySelectorAll('.navbar-nav>li>a').forEach((item) => {
+// rimuovere tutte le card
+
+let cards = document.querySelectorAll(".card");
+function remove() {
+	for (let i = 0; i < cards.length; i++) {
+	  cards[i].remove();
+	}
+  }
+//   remove();
+
+// contare i viaggi presenti sulla pagina
+
+console.log(cards.length);
+
+// chiusura navbar dopo il click
+
+const navbarLinks = document.querySelectorAll('.navbar-nav>li>a');
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  const item = navbarLinks[i];
+
   item.addEventListener('click', () => {
-	setTimeout(() => {
-	  const offCanvasElement = document.getElementById('offcanvasNavbar');
-	  const bsOffcanvas = bootstrap.Offcanvas.getInstance(offCanvasElement);
-	  
-	  if(bsOffcanvas) {
-		bsOffcanvas.hide();
-	  }
-	}, 500);
+    setTimeout(() => {
+      const offCanvasElement = document.getElementById('offcanvasNavbar');
+      const bsOffcanvas = bootstrap.Offcanvas.getInstance(offCanvasElement);
+
+      if (bsOffcanvas) {
+        bsOffcanvas.hide();
+      }
+    }, 500);
   });
-});
+}
