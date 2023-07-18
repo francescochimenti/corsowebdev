25c8a476fbb10b7ea87fc8970a85f7829d5a2833
francescochimenti <127688138+francescochimenti@users.noreply.github.com>
2023-07-18 20:17:50 +0200

    m4-d1-ok-no-extra

--- Corso/M4/D1/script.js
+++ Corso/M4/D1/script.js
@@ -16,4 +16,58 @@ function checkNum(num1, num2){
     return (num1 >= 40 && num1 <= 60 || num1 >= 70 && num1 <= 100 && num2 >= 40 && num2 <=60 || num2 >= 70 && num2 <= 100) ? true : false
 }
 console.log(checkNum(100, 100));
+
 //4
+function checkName(string){
+    return (string.toLowerCase().startsWith("los") || string.toLowerCase().startsWith("new")) ? string : false
+}
+console.log(checkName("New York"));
+
+//5
+myArr = [10,20,30,50,10,50,60,70,100,1]
+function myArrCalc(arr){
+    let somma = 0;
+    for (let i = 0; i < arr.length; i++){
+        somma += arr[i]
+    }
+    return somma
+}
+console.log(myArrCalc(myArr));
+
+//6
+function checkArr(arr){
+    for(let num of arr){
+        if (num === 1 || num === 3) {
+            return false
+        }
+    }
+    return true
+}
+console.log(checkArr(myArr));
+
+//7
+function angleChecker(num){
+    if(num < 90){
+        console.log("Angolo acuto");
+    } else if (num >= 91 && num <= 179){
+        console.log("Angolo ottuso");
+    } else if (num === 90){
+        console.log("Angolo retto");
+    } else {
+        console.log("Piatto");
+    }
+}
+angleChecker(100)
+
+//8
+function acronimo(string) {
+    let lettere = string.split(' ')
+    let myAcr = ''
+
+    for (let lettera of lettere) {
+        myAcr += lettera[0].toUpperCase();
+    }
+    return myAcr
+}
+console.log(acronimo("ciao come va?"));
+
